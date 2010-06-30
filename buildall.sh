81eade97d9594114e944898bd4ad446ad321b301
malc <av1474@comtv.ru>
2010-07-01 00:29:42 +0400

    "Auto build" lablGL too

--- buildall.sh
+++ buildall.sh
@@ -9,13 +9,16 @@ root=$(pwd)
 openjpeg=http://openjpeg.googlecode.com/svn/trunk/
 jbig2dec=git://git.ghostscript.com/jbig2dec.git
 sumatrapdf=http://sumatrapdf.googlecode.com/svn/trunk/
+lablgl=:pserver:anoncvs@camlcvs.inria.fr:/caml
 
 test -d openjpeg || svn checkout $openjpeg openjpeg
 test -d jbig2dec || git clone $jbig2dec jbig2dec
 test -d mupdf    || svn checkout $sumatrapdf/mupdf mupdf
+test -d lablgl   || cvs -d $lablgl co -d lablgl bazar-ocaml/lablGL
 
-mkdir -p $root/include
+mkdir -p $root/bin
 mkdir -p $root/lib
+mkdir -p $root/include
 
 (cd openjpeg \
     && make dist \
@@ -27,6 +30,15 @@ mkdir -p $root/lib
                              && ./configure --prefix=$root)) \
     && make install && rm $root/lib/*.so*)
 
+(cd lablgl \
+    && cat Makefile.config.linux.mdk > Makefile.config \
+    && make glut glutopt \
+    && make install \
+            BINDIR=$root/bin \
+            LIBDIR=$root/lib/ocaml \
+            DLLDIR=$root/lib/ocaml/stublibs \
+            INSTALLDIR=$root/lib/ocaml/lablGL)
+
 # OCaml has no cross compiler so uname is sufficient
 case $(uname -m) in
     ppc*|sparc*|arm*)
@@ -37,21 +49,21 @@ case $(uname -m) in
     ;;
 esac
 
+export CPATH=$CPATH:$root/include:$root/mupdf/mupdf:$root/mupdf/fitz
+export LIBRARY_PATH=$LIBRARY_PATH:$root/lib:$root/mupdf/build/release
+
 (cd mupdf && make build=release)
 
 cd ..
-
-export CPATH=$CPATH:$root/include:$root/mupdf/mupdf:$root/mupdf/fitz
-export LIBRARY_PATH=$CPATH:$root/lib:$root/mupdf/build/release
 
 srcpath=$(dirname $0)
 
 cclib="-lmupdf -lz -ljpeg -lopenjpeg -ljbig2dec -lfreetype"
 ocamlc -c -o link.o -ccopt -O $srcpath/link.c
-ocamlc -c -o main.cmo -I +lablGL $srcpath/main.ml
+ocamlc -c -o main.cmo -I $root/lib/ocaml/lablGL $srcpath/main.ml
 
 ocamlc -custom -o llpp \
--I +lablGL \
+-I $root/lib/ocaml/lablGL \
 str.cma unix.cma lablgl.cma lablglut.cma \
 link.o \
 -cclib "$cclib" \
