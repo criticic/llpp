addf79f750c7b9e5bf7c3daf58d5f87a44fd5222
malc <av1474@comtv.ru>
2011-08-08 09:00:23 +0400

    Tweak Down behavior in bird's eye

--- main.ml
+++ main.ml
@@ -1780,14 +1780,18 @@ let birdseyespecial key x y (conf, leftx, pageno, hooverpageno) =
   | Glut.KEY_DOWN ->
       let pageno = min (state.pagecount - 1) (pageno + 1) in
       state.birdseye <- Some (conf, leftx, pageno, hooverpageno);
-      if not (pagevisible state.layout pageno)
-      then
-        begin match List.rev state.layout with
-        | [] -> gotopage pageno 0.0
-        | l :: _ ->
-            gotoy (state.y + conf.interpagespace + l.pageh*2 - l.pagevh)
-        end
-      else Glut.postRedisplay ();
+      let rec loop = function
+        | [] -> gotopage1 pageno 0
+        | l :: rest ->
+            if l.pageno = pageno
+            then (
+              if l.pagevh != l.pageh
+              then gotoy (clamp (l.pageh - l.pagevh + conf.interpagespace))
+              else Glut.postRedisplay ()
+            )
+            else loop rest
+      in
+      loop state.layout
 
   | Glut.KEY_PAGE_UP ->
       begin match state.layout with
