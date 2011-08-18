353aaa099c2b295576c48e955ff5ff1ee8906583
malc <av1474@comtv.ru>
2011-08-18 19:04:48 +0400

    Fix parentheses

--- mkhelp.sh
+++ mkhelp.sh
@@ -1,4 +1,4 @@
 ocaml str.cma $1 $2
-v=$((cd $(dirname $1) && git describe --tags --dirty 2>/dev/null) \
+v=$(cd $(dirname $1) && git describe --tags --dirty 2>/dev/null \
     || echo "unknown")
 printf 'let version ="%s";;\n' $v
