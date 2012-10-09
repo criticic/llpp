028d844df7680f4ad1fef0eb6c8250be41ec7c84
malc <av1474@comtv.ru>
2012-10-09 10:34:09 +0400

    Do not store movable pointers

--- link.c
+++ link.c
@@ -3276,7 +3276,7 @@ CAMLprim value ml_init (value pipe_v, value params_v)
     mustoresize         = Int_val (Field (params_v, 5));
     colorspace          = Int_val (Field (params_v, 6));
     fontpath            = String_val (Field (params_v, 7));
-    state.trimcachepath = String_val (Field (params_v, 8));
+    state.trimcachepath = strdup (String_val (Field (params_v, 8)));
 
     state.ctx = fz_new_context (NULL, NULL, mustoresize);
 
