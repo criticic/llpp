41bc7b2f37ceb54bd499143a134d67b6198e8153
malc <av1474@comtv.ru>
2014-08-16 18:38:58 +0400

    Remove superfluous newline
    Thanks to Mark Oteiza for heads up.

--- mkhelp.sh
+++ mkhelp.sh
@@ -3,4 +3,4 @@ set -e
 cat $2 | ocaml str.cma $1
 v=$(cd $(dirname $1) && git describe --tags --dirty 2>/dev/null \
     || echo "unknown")
-echo "let version =\"$v\";;\n"
+echo "let version =\"$v\";;"
