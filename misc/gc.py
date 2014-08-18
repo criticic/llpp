4fc967506989680faf64fb6c1dd0e0d13c678124
malc <av1474@comtv.ru>
2014-08-19 01:44:52 +0400

    Simplify

--- misc/gc.py
+++ misc/gc.py
@@ -6,18 +6,19 @@ fields = string.split (sys.stdin.read (), '\x00')
 while len (fields) > 1:
     opath, atime = fields[0:2]
     fields = fields[2:]
+    npath = None
     if not os.path.exists (opath):
         try:
             npath = subprocess.check_output (
                 "locate -b -l 1 -e '/%s$'" % opath,
-                shell = True
+                shell = False
             )
         except:
-            npath = ""
-        if npath:
+            pass
+        if npath is not None:
             npath = npath[:-1]
-            sys.stdout.write (opath + "\000" + npath + "\000")
-        else:
+        sys.stdout.write (opath + "\000" + npath + "\000")
+    elif os.path.isdir (opath):
             sys.stdout.write (opath + "\000\000")
 
 sys.stdout.flush ()
