8dd21938c4e7b97e891bd919ca0fa15c2f197d3e
malc <av1474@comtv.ru>
2014-08-24 22:17:55 +0400

    Python3 compatibility

--- misc/cbzl.py
+++ misc/cbzl.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # based on http://doeidoei.wordpress.com/2010/11/23/compressing-files-with-python-symlink-trouble/
-import os, sys, zipfile, string
+import os, sys, zipfile
 
 if len (sys.argv) < 2:
-    print """\
+    print ("""\
 usage: find . -name \*.jpg -or -name \*.png -print0 | python cbzl.py curimgs.cbz
    or: locate -0 .jpg | python cbzl.py alljpegs.cbz
-"""
+""")
     sys.exit (1)
 
 with zipfile.ZipFile (sys.argv[1], "w") as z:
     zerosep = sys.stdin.read ()
-    files = string.split (zerosep, '\0')
+    files = zerosep.split ('\0')
     for name in files:
         a = zipfile.ZipInfo ()
         a.filename = os.path.basename (name)
         a.create_system = 3
-        a.external_attr = 2716663808L
+        a.external_attr = 2716663808
         z.writestr (a, name)
