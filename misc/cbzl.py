f01ee9f38d6d5da20c05267b76d4a172c3d38d2f
malc <av1474@comtv.ru>
2014-08-24 23:16:15 +0400

    Cosmetics

--- misc/cbzl.py
+++ misc/cbzl.py
@@ -2,11 +2,13 @@
 # based on http://doeidoei.wordpress.com/2010/11/23/compressing-files-with-python-symlink-trouble/
 import os, sys, zipfile
 
-if len (sys.argv) < 2:
-    print ("""\
+help = """\
 usage: find . -name \*.jpg -or -name \*.png -print0 | python cbzl.py curimgs.cbz
    or: locate -0 .jpg | python cbzl.py alljpegs.cbz
-""")
+"""
+
+if len (sys.argv) < 2:
+    print (help)
     sys.exit (1)
 
 with zipfile.ZipFile (sys.argv[1], "w") as z:
