7a6d7caf2866bd8e29659f49ebbc89dcfd9bd91e
malc <av1474@comtv.ru>
2014-08-30 17:36:25 +0400

    Cosmetics

--- mkhelp.sh
+++ mkhelp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cat $2 | ocaml str.cma $1
+ocaml str.cma $1 <$2
 v=$(cd $(dirname $1) && git describe --tags --dirty 2>/dev/null \
     || echo "unknown")
 echo "let version =\"$v\";;"
