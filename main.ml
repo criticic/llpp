e2107bd713936d835ff8eb7a0e8bf02e573642aa
malc <av1474@comtv.ru>
2014-12-21 17:41:16 +0300

    Restore trimming when visiting document via gotohist

--- main.ml
+++ main.ml
@@ -3443,6 +3443,7 @@ let gotohist (path, (c, bookmarks, x, anchor)) =
   state.origin <- E.s;
   setconf conf c;
   opendoc path E.s;
+  settrim conf.trimmargins conf.trimfuzz;
 ;;
 
 let gotooutline (_, _, kind) =
