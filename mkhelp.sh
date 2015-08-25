64ecd3862691585088752933efffb867c818c076
malc <moosotc@gmail.com>
2015-08-25 03:06:14 +0300

    Generate help.ml via shell/sed

--- mkhelp.sh
+++ mkhelp.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
-ocaml str.cma $1 <$2
-v=$(cd $(dirname $1) && git describe --tags --dirty 2>/dev/null \
-    || echo "unknown")
-echo "let version =\"$v\";;"
+v=$(cd $(dirname $0) && git describe --tags --dirty 2>/dev/null || echo unknown)
+sed "$1" -f - <<EOF
+1i\let keys = [
+s;\([^[:space:]]*\) *- \(.*\);\1\\\\t\2;
+s;-----\(.*\)-----;\\xc2\\xb7\1;
+s;";\\\\";g
+s;^;";
+s.$.";.
+\$a\];; let version = "$v";;
+EOF
