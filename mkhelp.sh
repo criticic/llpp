b1c00cd881c2be1c60faaab06aa1b7e49e65a275
malc <moosotc@gmail.com>
2015-08-31 04:30:27 +0300

    Avoid emiting control symbols

--- mkhelp.sh
+++ mkhelp.sh
@@ -3,11 +3,11 @@ set -e
 ocaml str.cma -stdin "$1" "$2" <<EOF
 let fixup = let open Str in
   let dash = regexp {|\([^ ]*\) +- +\(.*\)|}
-  and head = regexp {|-----\(.*\)-----|} in
-  fun s -> global_replace dash "\\\\1\\t\\\\2" s |>
-           global_replace head "\\xc2\\xb7\\\\1";;
+  and head = regexp {|-----\(.*\)-----|} in fun s ->
+  String.escaped s |> global_replace dash {|\1\t\2|}
+                   |> global_replace head {|\xc2\xb7\1|};;
 let rec iter ic = match input_line ic with
-| s -> Printf.printf "%S;\\n" @@ fixup s; iter ic
+| s -> Printf.printf "\"%s\";\\n" @@ fixup s; iter ic
 | exception End_of_file -> ();;
 Printf.printf "let keys = [\\n";
 iter @@ open_in Sys.argv.(1);;
