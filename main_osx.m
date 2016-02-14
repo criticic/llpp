f364c1d072dfc9416fe9fbe056bbb6c6eb3cb70d
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2016-02-15 00:20:49 +0100

    Improve os x driver

--- main_osx.m
+++ main_osx.m
@@ -8,6 +8,18 @@ int main (int argc, char **argv)
         [NSApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 
+        id menubar = [NSMenu new];
+        id appMenuItem = [NSMenuItem new];
+        [menubar addItem:appMenuItem];
+        [NSApp setMainMenu:menubar];
+        id appMenu = [NSMenu new];
+        id appName = [[NSProcessInfo processInfo] processName];
+        id quitTitle = [@"Quit " stringByAppendingString:appName];
+        id quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
+                                                      action:@selector(terminate:) keyEquivalent:@"q"];
+        [appMenu addItem:quitMenuItem];
+        [appMenuItem setSubmenu:appMenu];
+
         NSWindow *window =
             [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 400)
                                         styleMask:(NSTitledWindowMask | NSResizableWindowMask)
