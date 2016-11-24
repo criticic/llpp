0490d054f33f5c6a17ae889beedba8ba7f1084d4
malc <moosotc@gmail.com>
2016-11-24 07:15:48 +0300

    Hack is gone

--- Shakefile.hs
+++ Shakefile.hs
@@ -40,7 +40,7 @@ ocamlflagstbl = [("main", ("-I lablGL", "sed -f pp.sed", ["pp.sed"]))
 cflags = "-Wall -Werror -D_GNU_SOURCE -O\
          \ -g -std=c99 -pedantic-errors\
          \ -Wunused-parameter -Wsign-compare -Wshadow\
-         \ -DVISAVIS -DCSS_HACK_TO_READ_EPUBS_COMFORTABLY"
+         \ -DVISAVIS"
          ++ (if egl then " -DUSE_EGL" else "")
 cflagstbl =
   [("link.o"
