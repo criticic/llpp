0d84fa13e6ae8455e4cf98caaf408f1a0649272a
malc <moosotc@gmail.com>
2016-11-29 10:30:16 +0300

    Refactor

---
+++ keys.ml
@@ -0,0 +1,17 @@
+type pv =
+  [`B | `F | `G | `H | `N | `P | `Q | `S | `W
+   | `backspace | `delete
+   | `escape | `insert | `enter
+   | `kpdelete | `kpdown | `kpend | `kpenter | `kphome | `kpleft | `kpminus
+   | `kpnext | `kpplus | `kpprior | `kpright | `kpup
+   | `up | `down | `left | `minus | `next | `home | `prior | `jend
+   | `question | `right | `slash | `space
+   | `plus| `tab | `tilde | `equals | `pipe
+   | `F1 | `F3 | `F9
+   | `Gt | `Lt  | `Lb | `Rb
+   | `apos
+   | `_0 | `_1 | `_2 | `_3 | `_4 | `_5 | `_6 | `_7 | `_8 | `_9
+   | `a | `b | `c | `e | `f | `g | `h | `i | `j | `k | `l | `m
+   | `n | `o | `p | `q | `r | `s | `t | `u | `v | `w | `x | `y | `z
+   | `Code of int ]
+;;
