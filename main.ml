35113fbe37a385e8a886288a86cf970ac85e742e
Tobias Kortkamp <git@tobik.me>
2017-02-01 03:01:33 +0100

    llpp: Fix brightness increase key

--- main.ml
+++ main.ml
@@ -4983,7 +4983,7 @@ let viewkeyboard key mask =
 
   | Ascii ('['|']' as c) ->
      conf.colorscale <-
-       bound (conf.colorscale +. (if c = '>' then 0.1 else -0.1)) 0.0 1.0;
+       bound (conf.colorscale +. (if c = ']' then 0.1 else -0.1)) 0.0 1.0;
      G.postRedisplay "brightness";
 
   | Ascii 'c' when state.mode = View ->
