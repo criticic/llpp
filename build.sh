0b2d3e67b217464ed1886ca60c0671945cc0f3b4
malc <moosotc@gmail.com>
2018-04-08 12:49:53 +0300

    Improve

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-trap 'test $? -eq 0 || echo build failed' EXIT
-
 date --help | grep -q GNU-date-that-actually-works-with-N && {
     now() { date +%N; }
     scl=1000000000.0
@@ -10,58 +8,49 @@ date --help | grep -q GNU-date-that-actually-works-with-N && {
     now() { date +%s; }
     scl=1
 }
+
 tstart=$(now)
+
+partmsg() {
+    test $? -eq 0 && msg="pokatit" || msg="fignja"
+    echo "$msg $(echo "scale=3; ($(now) - $tstart) / $scl" | bc -l)"
+}
 
 die() {
     echo "$*" >&2
     exit 111
 }
 
+trap 'partmsg' EXIT
+
 test -n "$1" || die "usage: $0 build-directory"
 
 outd=$1
 srcd=$PWD
 
-getpast() {
-    past="$1.past"
-    cur_cmd="$2"
-    key_cmd="$3"
-    test -r $past && {
-        . $past
-        test "$cmd" = "$cur_cmd" && {
-            eval "cur_key=\$($key_cmd)" || dirty=nokey && {
-                    test "$cur_key" = "$key" || dirty="key"
-                }
-        } || dirty="cmd"
-    } || dirty="initial"
+isfresh() {
+    test -e "$1" && test -r "$1.past" && {
+        . "$1.past" test "$k" = "$2" || echo "key mismatch\n '$k'\n '$2'"
+    }
 }
 
 bocaml1() {
-    s="$1"
-    o="$2"
     eval ocamlc -depend -bytecode -one-line $incs $s | {
         read _ _ depl
         test -z "$depl" || {
             for d in $(eval echo $depl); do
                 d=${d#$srcd/}
-                bocaml $d $((n+1)) || dirty=transitive
+                bocaml $d $((n+1))
             done
         }
     }
     cmd="ocamlc $incs -c -o $o $s"
-    keycmd="stat -c %Y $o $s 2>/dev/null | tr -d '\n'"
-    getpast "$o" "$cmd" "$keycmd"
-    test -n "$dirty" && {
-        printf "%*.s%s -> %s\n" $n '' \
-               "${s#$srcd/}" "${o#$outd/} [${dirty-fresh}]"
-        eval "$cmd" || die "compilation failed"
-        eval "key=\$($keycmd)"
-        printf "cmd='$cmd'\nkey='$key'\n" >$o.past
-        grep -q "$o" $outd/ordered || echo "$o" >>$outd/ordered
-        return 1
-    } || {
-        grep -q "$o" $outd/ordered || echo "$o" >>$outd/ordered
-        return 0
+    keycmd="stat -c %Y $o $s"
+    grep -q "$o" $outd/ordered || echo "$o" >>$outd/ordered
+    isfresh "$o" "'$cmd$(eval $keymd)'" || {
+        printf "%*.s%s -> %s\n" $n '' "${s#$srcd/}" "$o"
+        eval "$cmd"
+        echo "k='$cmd$(eval $keycmd)'" >$o.past
     }
 }
 
@@ -69,20 +58,16 @@ bocaml() (
     o="$1"
     n="$2"
     wocmi="${o%.cmi}"
-    test ${wocmi%help.cmo} !=  ${wocmi} && {
+    test ${wocmi%help.cmo} !=  $wocmi && {
         s=$outd/help.ml
         o=$outd/help.cmo
     } || {
-        test "$o" = "$wocmi" && {
-            s=${o%.cmo}.ml
-        } || {
-            s=$wocmi.mli
-        }
+        test "$o" = "$wocmi" && s=${o%.cmo}.ml || s=$wocmi.mli
         s=$srcd/$s
         o=$outd/$o
     }
     incs="-I lablGL -I $outd/lablGL -I wsi/x11 -I $outd/wsi/x11 -I $outd"
-    bocaml1 "$s" "$o" || return 1 && return 0
+    bocaml1 "$s" "$o"
 )
 
 bocamlc() {
@@ -91,24 +76,19 @@ bocamlc() {
     mudir=$srcd/mupdf
     muinc="-I $mudir/include -I $mudir/thirdparty/freetype/include"
     cmd="ocamlc -ccopt \"-O2 $muinc -o $o\" $s"
-    keycmd="stat -c %Y $o $s 2>/dev/null | tr -d '\n'"
-    getpast "$o" "$cmd" "$keycmd"
-    test -n "$dirty" && {
-        printf "%s -> %s\n" "${s#$srcd/}" "${o#$outd/} [${dirty-fresh}]"
-        eval "$cmd" || die "compilation failed"
-        eval "key=\$($keycmd)" || die "$keycmd failed"
-        printf "cmd='$cmd'\nkey='$key'\n" >$o.past
-        return 1
-    } || return 0
+    keycmd="stat -c %Y $o $s 2>/dev/null"
+    isfresh "$o" "'$cmd$(eval $keycmd)'" || {
+        printf "%s -> %s\n" "${s#$srcd/}" "$o"
+        eval "$cmd"
+        echo "k='$cmd$(eval $keycmd)'" >$o.past
+    }
 }
 
 mkdir -p $outd/wsi/x11
 mkdir -p $outd/lablGL
-relink=0
 :>$outd/ordered
 
 mkhelp() {
-    vers=$(cd $srcd && git describe --tags || echo unknown)
     ocaml str.cma -stdin $srcd/KEYS <<EOF
 let fixup = let open Str in
   let dash = regexp {|\([^ ]*\) +- +\(.*\)|}
@@ -120,40 +100,35 @@ let rec iter ic = match input_line ic with
 | exception End_of_file -> ();;
 Printf.printf "let keys = [\\n";
 iter @@ open_in Sys.argv.(1);;
-Printf.printf "] and version = \"$vers\"";;
+Printf.printf "] and version = \"$ver\"";;
 EOF
 }
 
+ver=$(cd $srcd && git describe --tags || echo unknown)
 cmd="mkhelp >$outd/help.ml"
-keycmd="stat -c %Y $srcd/KEYS 2>/dev/null"
-getpast "$outd/help.ml" "$cmd" "$keycmd"
-test -n "$dirty" && { eval $cmd || die "mkhelp failed"; relink=1; }
-eval "key=\$($keycmd)" || die "$keycmd: failed"
-printf "cmd='$cmd'\nkey='$key'\n" >$outd/help.ml.past
+keycmd="stat -c %Y $srcd/KEYS"
+isfresh "$outd/help.ml" "'$cmd$(eval $keycmd)$ver'" || {
+    eval $cmd
+    echo "k='$cmd$(eval $keycmd)$ver'" >$outd/help.ml.past
+}
 
 for m in lablGL/glMisc.cmo lablGL/glTex.cmo wsi/x11/wsi.cmo main.cmo; do
-    bocaml $m 0 || relink=1
+    bocaml $m 0
 done
-bocamlc link.o || relink=1
+bocamlc link.o
 
 libs="str.cma unix.cma"
 clibs="-lGL -lX11 -L$mudir/build/native -lmupdf -lmupdfthird -lpthread"
 globjs=
 for f in ml_gl ml_glarray ml_raw; do
-    bocamlc lablGL/$f.o || relink=1
+    bocamlc lablGL/$f.o
     globjs="$globjs $outd/lablGL/$f.o"
 done
 
-ord=$(grep -v \.cmi $outd/ordered | tr "\n" " ")
+ord=$(echo $(eval grep -v \.cmi $outd/ordered))
 cmd="ocamlc -custom $libs -o $outd/llpp $ord"
 cmd="$cmd $globjs $outd/link.o -cclib \"$clibs\""
-keycmd="stat -c %Y $outd/llpp $ord 2>/dev/null"
-getpast "$outd/llpp" "$cmd" "$keycmd"
-test $relink -ne 0 || test -n "$dirty" && {
-        echo "linking"
+isfresh "$outd/llpp" "'$cmd$(eval $keycmd)'" && echo fresh || {
         eval $cmd
-        eval "key=\$($keycmd)" || die "$keycmd: failed"
-        printf "cmd='$cmd'\nkey='$key'\n" >$outd/llpp.past
-    } || echo "nothing to be done"
-
-printf "took %s sec\n" $(echo "scale=3; ($(now) - $tstart) / $scl" | bc -l)
+        echo "k='$cmd$(eval $keycmd)'" >$outd/llpp.past
+    }
