14bd8ae384e9b50560117ad554ebd2f7c7039ddb
malc <moosotc@gmail.com>
2018-05-12 09:20:03 +0300

    Cosmetics

--- utils.ml
+++ utils.ml
@@ -339,10 +339,9 @@ let r32s s pos =
   let rb pos1 = Char.code (Bytes.get s (pos + pos1)) in
   let v0 = rb 0 and v1 = rb 1 and v2 = rb 2 and v3 = rb 3 in
   let v = v0 lor (v1 lsl 8) lor (v2 lsl 16) lor (v3 lsl 24) in
-  if v3 land 0x80 = 0 then
-    v
-  else
-    (v - (1 lsl 32))
+  if v3 land 0x80 = 0
+  then v
+  else (v - (1 lsl 32))
 ;;
 
 let vlog fmt = Format.ksprintf ignore fmt;;
