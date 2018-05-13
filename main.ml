579d47f43ac98846b1c72abed9046657febc79d0
malc <moosotc@gmail.com>
2018-05-13 12:40:58 +0300

    Cosmetics

--- main.ml
+++ main.ml
@@ -11,8 +11,9 @@ external markunder : opaque -> int -> int -> mark -> bool = "ml_markunder";;
 external clearmark : opaque -> unit = "ml_clearmark";;
 external zoomforh : int -> int -> int -> int -> float = "ml_zoom_for_height";;
 external getmaxw : unit -> float = "ml_getmaxw";;
-external postprocess : opaque -> int -> int -> int -> (int * string * int)
-                       -> int = "ml_postprocess";;
+external postprocess :
+  opaque -> int -> int -> int -> (int * string * int) -> int
+  = "ml_postprocess";;
 external pagebbox : opaque -> (int * int * int * int) = "ml_getpagebox";;
 external setaalevel : int -> unit = "ml_setaalevel";;
 external realloctexts : int -> bool = "ml_realloctexts";;
@@ -25,14 +26,14 @@ external getpbo : width -> height -> colorspace -> opaque = "ml_getpbo";;
 external freepbo : opaque -> unit = "ml_freepbo";;
 external unmappbo : opaque -> unit = "ml_unmappbo";;
 external bousable : unit -> bool = "ml_bo_usable";;
-external unproject : opaque -> int -> int
-                     -> (int * int) option = "ml_unproject";;
-external project : opaque -> int -> int -> float -> float
-                   -> (float * float) = "ml_project";;
-external drawtile : tileparams -> opaque
-                    -> unit = "ml_drawtile";;
-external rectofblock : opaque -> int -> int
-                       -> float array option = "ml_rectofblock";;
+external unproject :
+  opaque -> int -> int -> (int * int) option = "ml_unproject";;
+external project :
+  opaque -> int -> int -> float -> float -> (float * float) = "ml_project";;
+external drawtile :
+  tileparams -> opaque -> unit = "ml_drawtile";;
+external rectofblock :
+  opaque -> int -> int -> float array option = "ml_rectofblock";;
 external begintiles : unit -> unit = "ml_begintiles";;
 external endtiles : unit -> unit = "ml_endtiles";;
 external addannot : opaque -> int -> int -> string -> unit = "ml_addannot";;
@@ -40,14 +41,14 @@ external modannot : opaque -> slinkindex -> string -> unit = "ml_modannot";;
 external delannot : opaque -> slinkindex -> unit = "ml_delannot";;
 external hasunsavedchanges : unit -> bool = "ml_hasunsavedchanges";;
 external savedoc : string -> unit = "ml_savedoc";;
-external getannotcontents : opaque -> slinkindex
-                            -> string = "ml_getannotcontents";;
-external drawprect : opaque -> int -> int -> float array
-                     -> unit = "ml_drawprect";;
+external getannotcontents :
+  opaque -> slinkindex -> string = "ml_getannotcontents";;
+external drawprect :
+  opaque -> int -> int -> float array -> unit = "ml_drawprect";;
 external wcmd : Unix.file_descr -> bytes -> int -> unit = "ml_wcmd";;
 external rcmd : Unix.file_descr -> string = "ml_rcmd";;
-external uritolocation : string
-                         -> (pageno * float * float) = "ml_uritolocation";;
+external uritolocation :
+  string -> (pageno * float * float) = "ml_uritolocation";;
 external isexternallink : string -> bool = "ml_isexternallink";;
 
 (* copysel _will_ close the supplied descriptor *)
