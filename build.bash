f547c94f1e7c10f0f62e0538a96c061cf905521e
malc <moosotc@gmail.com>
2018-05-15 17:00:25 +0300

    Remove ccache
    User might not have it installed.

--- build.bash
+++ build.bash
@@ -48,7 +48,7 @@ isfresh() {
 mulibs="$mudir/build/native/libmupdf.a" # $mudir/build/native/libmupdf-third.a
 keycmd="(cd $mudir && git describe --tags --dirty); digest $mulibs"
 isfresh "$mulibs" "$(eval $keycmd)" || (
-    make -C "$mudir" CC='ccache gcc' build=native -j $mjobs libs
+    make -C "$mudir" build=native -j $mjobs libs
     echo "k=\"$(eval $keycmd)\"" >$mudir/build/native/libmupdf.a.past
 ) && vecho "fresh mupdf"
 
