4a1ee0d5034b3fa4f384966c00485d46f37b661a
malc <moosotc@gmail.com>
2018-05-16 22:12:11 +0300

    Untabify and indent

--- misc/getmupdf.bash
+++ misc/getmupdf.bash
@@ -5,8 +5,8 @@ MUPDF_URL="https://github.com/ArtifexSoftware/mupdf"
 MUPDF_DESIRED_VERSION="bd88b96f7b7b21d64d4fb32a2674653a08f8cb38"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
-	echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
-	git clone ${MUPDF_URL} --recursive ${MUPDF_OUTPUT_DIR}
+    echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
+    git clone ${MUPDF_URL} --recursive ${MUPDF_OUTPUT_DIR}
 fi
 
 cd ${MUPDF_OUTPUT_DIR}
@@ -14,7 +14,7 @@ git pull
 MUPDF_VERSION=$(git rev-parse HEAD)
 
 if [ "${MUPDF_VERSION}" = "${MUPDF_DESIRED_VERSION}" ]; then
-	exit 0
+    exit 0
 fi
 
 echo "mupdf current version is ${MUPDF_VERSION} switching to ${MUPDF_DESIRED_VERSION}"
