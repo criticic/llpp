be8da80eed75bd29596709e8b3f5b755e8520f81
malc <moosotc@gmail.com>
2018-05-19 07:07:23 +0300

    Safety net and clone args

--- misc/getmupdf.bash
+++ misc/getmupdf.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 MUPDF_OUTPUT_DIR="$1"
 MUPDF_URL="https://github.com/ArtifexSoftware/mupdf"
@@ -6,7 +7,7 @@ MUPDF_DESIRED_VERSION="bd88b96f7b7b21d64d4fb32a2674653a08f8cb38"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
-    git clone ${MUPDF_URL} --recursive ${MUPDF_OUTPUT_DIR}
+    git clone ${cloneargs} ${MUPDF_URL} --recursive ${MUPDF_OUTPUT_DIR}
 fi
 
 cd ${MUPDF_OUTPUT_DIR}
