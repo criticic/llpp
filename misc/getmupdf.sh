c7ec008b677f6b320fe0ed413835ca23b9ae9e98
malc <moosotc@gmail.com>
2018-10-27 11:42:59 +0300

    Sync with upstream
    This allows fitz to work (again) with humongous images, plus makes it
    spew out warning when it can't.
    
    diff --git a/source/fitz/draw-affine.c b/source/fitz/draw-affine.c
    index 04f02f83e..5cf7adf7d 100644
    --- a/source/fitz/draw-affine.c
    +++ b/source/fitz/draw-affine.c
    @@ -6,7 +6,7 @@
     #include <assert.h>
    
     /* Number of fraction bits for fixed point math */
    -#define PREC 14
    +#define PREC 12
     #define MASK ((1<<PREC)-1)
     #define ONE (1<<PREC)
     #define HALF (1<<(PREC-1))

--- misc/getmupdf.sh
+++ misc/getmupdf.sh
@@ -3,7 +3,7 @@ set -eu
 
 MUPDF_OUTPUT_DIR="$1"
 MUPDF_URL="https://github.com/ArtifexSoftware/mupdf"
-MUPDF_DESIRED_VERSION="a64b8e6c4a26b4e73e96f9b922969c37989acf59"
+MUPDF_DESIRED_VERSION="cb530cdc26f5e04601f00140da02299b4b59a55b"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
