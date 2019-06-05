e9232e7da8ef2b120d380ed3597dace98be46d0c
malc <moosotc@gmail.com>
2019-06-05 18:32:54 +0300

    Cosmetics

--- main.ml
+++ main.ml
@@ -1381,9 +1381,7 @@ let act cmds =
                sub 10 2 ':';
                sub 12 2 ':';
                sub 14 2 ' ';
-               Buffer.add_char b '[';
-               Buffer.add_string b v;
-               Buffer.add_char b ']';
+               Printf.bprintf b  "[%s]" v;
                Buffer.contents b
              else args
            )
