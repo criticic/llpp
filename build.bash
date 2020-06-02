46aa42f2d1e74d94c56c3d461a70b81ae9d50075
malc <moosotc@gmail.com>
2020-06-02 15:46:14 +0300

    Add noexecstack bandaid
    Otherwise _I_ get:
    [ 3175.205117] process 'xsrc/llpp/build/llpp' started with executable stack
    in the kernel log (dmesg (1)).

--- build.bash
+++ build.bash
@@ -299,6 +299,7 @@ fi
 
 ord=$(grep -v \.cmi $outd/ordered)
 cmd="ocamlc -custom $libs -o $outd/llpp $cobjs $(echo $ord) -cclib \"$clibs\""
+cmd="$cmd -ccopt -Wl,-z,noexecstack"
 keycmd="digest $outd/llpp $cobjs $ord $mulibs"
 isfresh "$outd/llpp" "$cmd$(eval $keycmd)" || {
     echo linking $outd/llpp
