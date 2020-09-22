aa127082febcdb1fd56c5971b552d9a942a57145
Junnan Zhang <zhangjunnan1224@gmail.com>
2020-09-22 22:46:19 +0800

    Make gcext.py accept arguments

--- misc/gcext.py
+++ misc/gcext.py
@@ -1,19 +1,58 @@
+import argparse
+import os
 import xml.etree.ElementTree as ET
-from datetime import date
-tree = ET.parse('llpp.conf')
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-i',
+    dest='input',
+    type=str,
+    metavar='SOURCE',
+    help='path to your llpp.conf; default to ~/.config/llpp.conf',
+    default='~/.config/llpp.conf')
+parser.add_argument(
+    '-d',
+    dest='timestamp',
+    type=int,
+    help='entries saved earlier than this date will be removed')
+parser.add_argument(
+    '-k',
+    dest='keyword',
+    type=str,
+    help='entries with path matching the keyword will be removed')
+parser.add_argument(dest='dest',
+                    type=str,
+                    metavar='DEST',
+                    help='path to output config file')
+
+args = parser.parse_args()
+source = args.input
+cts = args.timestamp
+keyword = args.keyword
+dest = args.dest
+
+tree = ET.parse(os.path.expanduser(source))
 root = tree.getroot()
 
+print('Removing the following entries in llpp.conf:')
+i = 0
 for doc in root.findall('doc'):
     # skip entries with bookmarks
     if doc.find('bookmarks'):
         continue
     # remove entries older than 2019.9.1
     ts = int(doc.get('last-visit'))
-    dt = date.fromtimestamp(ts)
-    if dt < date(2019, 9, 1):
+    if cts is not None and ts < cts:
+        print(doc.get('path'))
         root.remove(doc)
+        i += 1
+        continue
     # remove entries whose path contains the keyword
-    if "keyword" in doc.get('path'):
+    if keyword is not None and keyword in doc.get('path'):
+        print(doc.get('path'))
         root.remove(doc)
+        i += 1
 
-tree.write('llpp_new.conf')
+print(i, 'entries have been removed.',
+      'The new configuration file has been saved to', dest)
+tree.write(dest)
