89f5bd836b0cd93b2240990342bb5f035ea3c730
malc <moosotc@gmail.com>
2020-10-31 04:40:37 +0300

    Remove indirection

--- utils.ml
+++ utils.ml
@@ -33,7 +33,7 @@ let exntos = function
   | exn -> Printexc.to_string exn
 ;;
 
-let error fmt = Printf.kprintf (fun s -> failwith s) fmt;;
+let error fmt = Printf.kprintf failwith fmt;;
 
 module IntSet = Set.Make (struct type t = int let compare = (-) end);;
 
