6f0f32df91523204e39eda000c58cea203d53a92
malc <clamky@hotmail.com>
2020-11-18 15:35:06 +0300

    Use Bytes provided binary integer reading helpers
    NB Need to test on Blefuscan and 32bit sides.

--- utils.ml
+++ utils.ml
@@ -274,27 +274,15 @@ let substratis s pos subs =
   else false
 ;;
 
-let w8 s pos i = Bytes.set s pos (Char.chr (i land 0xff));;
-let r8 s pos = Char.code (Bytes.get s pos);;
-
-let w16 s pos i =
-  w8 s pos i;
-  w8 s (pos+1) (i lsr 8)
-;;
+let w8 = Bytes.set_uint8;;
+let r8 = Bytes.get_uint8;;
+let w16 = Bytes.set_uint16_le;;
+let r16 = Bytes.get_uint16_le;;
+let r16s = Bytes.get_int16_le;;
 
 let w32 s pos i =
   w16 s pos i;
   w16 s (pos+2) (i lsr 16)
-;;
-
-let r16 s pos =
-  let rb pos1 = Char.code (Bytes.get s (pos + pos1)) in
-  (rb 0) lor ((rb 1) lsl 8)
-;;
-
-let r16s s pos =
-  let i = r16 s pos in
-  i - ((i land 0x8000) lsl 1)
 ;;
 
 let r32 s pos =
