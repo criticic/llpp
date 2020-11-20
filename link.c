fc1a81e668c686c33f7bc511583b74ac5ca9d559
malc <clamky@hotmail.com>
2020-11-20 14:22:51 +0300

    Test presence of non power of two extension in non rectangular case

--- link.c
+++ link.c
@@ -3569,10 +3569,15 @@ ML (init (value csock_v, value params_v))
     int ret, texcount, colorspace, mustoresize;
     const char *fontpath;
 
-#if TEXT_TYPE != GL_TEXTURE_2D
+#if TEXT_TYPE == GL_TEXTURE_2D
+    if (!strstr ((const char *) glGetString (GL_EXTENSIONS),
+                 "texture_non_power_of_two")) {
+        errx (1, "OpenGL does not support NPOT textures");
+    }
+#else
     if (!strstr ((const char *) glGetString (GL_EXTENSIONS),
                  "texture_rectangle")) {
-        errx (1, "OpenGL does not support rectangular texture extension");
+        errx (1, "OpenGL does not support rectangularar textures");
     }
 #endif
     state.csock         = Int_val (csock_v);
