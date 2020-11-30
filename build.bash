86df5e611e69634ff846692aa7c8693fccb473ce
malc <clamky@hotmail.com>
2020-11-30 07:34:54 +0300

    Improve (on "unknown") version string

--- build.bash
+++ build.bash
@@ -246,7 +246,7 @@ bobjc() {
     } && vecho "fresh $o"
 }
 
-ver=$(cd $srcd && git describe --tags --dirty) || ver=unknown
+ver=$(cd $srcd && git describe --tags --dirty) || ver="'built on $(date -I)'"
 
 gen=$srcd/genconfstruct.sh
 cmd="(export print paste clip uriop; . $gen >$outd/confstruct.ml)"
