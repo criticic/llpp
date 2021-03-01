57d953e9e84d8ad10b6e556e3a396f2a5bcd39e8
malc <clamky@hotmail.com>
2021-03-01 23:46:49 +0300

    Papper over #160
    PDF attached to the bug report causes MuPDF to emit a lot of
    diagnostic messages like
      warning: FT_Get_Advance(HiddenHorzOCR,32534): invalid glyph index
    
    This happens inside ml_whatsunder which calls emsg an the same thread
    that drains the socket thus causing a deadlock. The solution is either
    calling back OCaml from C, not a fan, or doing nothing, which is the
    current "solution".

--- link.c
+++ link.c
@@ -45,6 +45,7 @@
 
 #include "cutils.h"
 
+#define FIXED_DIAG_DEADLOCK 0
 #define ARSERT(c) !(c) ? errx (1, "%s:%d " #c, __FILE__, __LINE__) : (void) 0
 #define ML(d) extern value ml_##d; value ml_##d
 #define ML0(d) extern void ml_##d; void ml_##d
@@ -3436,10 +3437,12 @@ ML (llpp_version (void))
     return caml_copy_string (llpp_version);
 }
 
+#if FIXED_DIAG_DEADLOCK
 static void diag_callback (void *user, const char *message)
 {
     printd ("emsg %s %s", (char *) user, message);
 }
+#endif
 
 static fz_font *lsff (fz_context *ctx,int UNUSED_ATTR script,
                       int UNUSED_ATTR language, int UNUSED_ATTR serif,
@@ -3533,8 +3536,10 @@ ML (init (value csock_v, value params_v))
 
     state.ctx = fz_new_context (NULL, NULL, mustoresize);
     fz_register_document_handlers (state.ctx);
+#if FIXED_DIAG_DEADLOCK
     fz_set_error_callback (state.ctx, diag_callback, "[e]");
     fz_set_warning_callback (state.ctx, diag_callback, "[w]");
+#endif
     fz_install_load_system_font_funcs (state.ctx, NULL, NULL, lsff);
 
     state.trimmargins = Bool_val (Field (trim_v, 0));
