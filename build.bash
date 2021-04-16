criticic/llpp

commit c6fc00b5e88813e0510b2ae76fb838c21648d36d

Build system work
Interface files are:
 a) autogenerated by ocamlc -i
 b) taken from upstream in case of lablGL

$ gmk=1 bash build.bash build

will produce build/Makefile, that demonstrates that make(1)
parallelization produces noticable speedup (~2x) (interface files are
crucial for this)

build.bash
@@ -147,7 +147,6 @@ read cvers < <($ccomp --version)
 
 seen=
 ord=
-link=
 $gmk || :>$outd/Makefile
 bocaml1() {
     [[ ! $seen =~ $3 ]] || return 0
@@ -168,9 +167,8 @@ bocaml1() {
     } && vecho "fresh $o.depl"
 
     while read d; do
-        ord=
         bocaml $d $((n+1))
-        deps+=" $ord $outd/$d"
+        deps+=" $d"
     done <$o.depl
 
     cmd="ocamlc $(oflags $o) -c -o $o $s"
@@ -180,10 +178,15 @@ bocaml1() {
         eval "$cmd" || die "$cmd failed"
         echo "$overs$cmd$(eval $keycmd)" >"$o.past"
     } && vecho "fresh $o"
-    $gmk || printf "$o:$deps\n\t$cmd\n" >>$outd/Makefile
-
-    ord=$deps
+    $gmk || {
+        printf "$o:"
+        for dep in $deps; do
+            printf " $outd/$dep"
+        done
+        printf "\n\t%s\n" "$cmd"
+    } >>$outd/Makefile
     seen+=$o
+    ord+=" $o"
 }
 
 cycle=
@@ -258,24 +261,39 @@ for target; do
     esac
 done
 
-seen1=
-f() {
-    local o wocmi
-    [[ ! "$seen1" =~ "$1" ]] || return 0
-    seen1+="$1"
+flatten() {
+    local o wocmi ord=
+    [[ ! "$seen" =~ "$1" ]] || return 0
     bocaml $1 0
     for o in $ord; do
         case $o in
             *.cmi)
                 wocmi=${o%.cmi}
-                f ${wocmi#$outd/}.cmo;;
-            *.cmo) f ${o#$outd/};;
+                flatten ${wocmi#$outd/}.cmo;;
+            *.cmo) flatten ${o#$outd/};;
         esac
     done
-    [[ "link" =~ "$1" ]] || link+=" $outd/$1"
 }
+flatten main.cmo
 
-f main.cmo
+modules=
+collectmodules() {
+    local dep cmo this=$1
+    while read dep; do
+        case $dep in
+            *.cmi)
+                cmo=${dep%.cmi}.cmo
+                test $cmo = $this || collectmodules $cmo
+                ;;
+            *.cmo)
+                collectmodules $dep
+                cmo=$dep
+                ;;
+        esac
+        [[ $modules =~ $cmo ]] || modules+=" $outd/$cmo"
+    done <$outd/$1.depl
+}
+collectmodules main.cmo
 
 cobjs=
 for m in link cutils version; do
@@ -301,14 +319,14 @@ else
     bocamlc wsi/x11/xlib.o
 fi
 
-cmd="ocamlc -custom $libs -o $outd/llpp $cobjs $link -cclib \"$clibs\""
-keycmd="digest $outd/llpp $cobjs $link $mulibs"
+cmd="ocamlc -custom $libs -o $outd/llpp $cobjs $modules -cclib \"$clibs\""
+keycmd="digest $outd/llpp $cobjs $modules $mulibs"
 isfresh "$outd/llpp" "$cmd$(eval $keycmd)" || {
     echo linking $outd/llpp
     eval "$cmd" || die "$cmd failed"
     echo "$cmd$(eval $keycmd)" >"$outd/llpp.past"
 } && vecho "fresh llpp"
-$gmk || printf "$outd/llpp: $cobjs $link $mulibs\n\t$cmd\n" >>$outd/Makefile
+$gmk || printf "$outd/llpp: $cobjs $modules $mulibs\n\t$cmd\n" >>$outd/Makefile
 
 if $darwin; then
     out="$outd/llpp.app/Contents/Info.plist"

.gitignore
@@ -1,5 +1,7 @@
 build/
 test/
 patches/
+mli1/
+mli/
 .gdb_history
 .tramp_history

ffi.mli
@@ -1,0 +1,73 @@
+type initparams =
+    Config.angle * Config.fitmodel * Config.trimparams * Config.texcount *
+    Config.sliceheight * Config.memsize * Config.colorspace *
+    Config.fontpath * Config.redirstderr
+and xoff = int
+and yoff = int
+and noff = int
+and li = noff * string * hintfontsize * hintchars
+and hlmask = int
+and hintchars = string
+and hintfontsize = int
+external init : Unix.file_descr -> initparams -> Unix.file_descr = "ml_init"
+external seltext : Config.opaque -> int * int * int * int -> unit
+  = "ml_seltext"
+external hassel : Config.opaque -> bool = "ml_hassel"
+external getpdimrect : int -> float array = "ml_getpdimrect"
+external whatsunder : Config.opaque -> Config.x -> Config.y -> Config.under
+  = "ml_whatsunder"
+external markunder :
+  Config.opaque -> Config.x -> Config.y -> Config.mark -> bool
+  = "ml_markunder"
+external clearmark : Config.opaque -> unit = "ml_clearmark"
+external zoomforh : int -> int -> int -> int -> float = "ml_zoom_for_height"
+external getmaxw : unit -> float = "ml_getmaxw"
+external postprocess : Config.opaque -> hlmask -> xoff -> yoff -> li -> noff
+  = "ml_postprocess"
+external setdcf : string -> unit = "ml_setdcf"
+external pagebbox : Config.opaque -> Config.irect = "ml_getpagebox"
+external setaalevel : int -> unit = "ml_setaalevel"
+external setpapercolor : Config.rgba -> unit = "ml_setpapercolor"
+external realloctexts : int -> bool = "ml_realloctexts"
+external findlink : Config.opaque -> Config.linkdir -> Config.link
+  = "ml_findlink"
+external getlink : Config.opaque -> int -> Config.under = "ml_getlink"
+external getlinkn : Config.opaque -> string -> string -> int -> int
+  = "ml_getlinkn"
+external getlinkrect : Config.opaque -> int -> Config.irect
+  = "ml_getlinkrect"
+external findpwl : int -> int -> Config.pagewithlinks
+  = "ml_find_page_with_links"
+external unproject : Config.opaque -> int -> int -> (int * int) option
+  = "ml_unproject"
+external project :
+  Config.opaque -> int -> int -> float -> float -> float * float
+  = "ml_project"
+external drawtile : Config.tileparams -> Config.opaque -> unit
+  = "ml_drawtile"
+external rectofblock : Config.opaque -> int -> int -> float array option
+  = "ml_rectofblock"
+external begintiles : unit -> unit = "ml_begintiles"
+external endtiles : unit -> unit = "ml_endtiles"
+external addannot : Config.opaque -> int -> int -> string -> unit
+  = "ml_addannot"
+external modannot : Config.opaque -> Config.slinkindex -> string -> unit
+  = "ml_modannot"
+external delannot : Config.opaque -> Config.slinkindex -> unit
+  = "ml_delannot"
+external hasunsavedchanges : unit -> bool = "ml_hasunsavedchanges"
+external savedoc : string -> unit = "ml_savedoc"
+external getannotcontents : Config.opaque -> Config.slinkindex -> string
+  = "ml_getannotcontents"
+external wcmd : Unix.file_descr -> bytes -> int -> unit = "ml_wcmd"
+external rcmd : Unix.file_descr -> string = "ml_rcmd"
+external uritolocation : string -> Config.pageno * float * float
+  = "ml_uritolocation"
+external isexternallink : string -> bool = "ml_isexternallink"
+external copysel : Unix.file_descr -> Config.opaque -> unit = "ml_copysel"
+external drawstr : int -> int -> int -> string -> float = "ml_draw_string"
+external fz_version : unit -> string = "ml_fz_version"
+external llpp_version : unit -> string = "ml_llpp_version"
+external measurestr : int -> string -> float = "ml_measure_string"
+external toutf8 : int -> string = "ml_keysymtoutf8"
+external mbtoutf8 : string -> string = "ml_mbtoutf8"

glutils.mli
@@ -1,0 +1,11 @@
+val vraw : [< Raw.kind > `float ] Raw.t
+val filledrect2 :
+  float ->
+  float -> float -> float -> float -> float -> float -> float -> unit
+val filledrect1 : float -> float -> float -> float -> unit
+val filledrect : float -> float -> float -> float -> unit
+val linerect : float -> float -> float -> float -> unit
+val drawstring : int -> int -> int -> string -> unit
+val drawstringf : int -> int -> int -> ('a, unit, string, unit) format4 -> 'a
+val redisplay : bool ref
+val postRedisplay : string -> unit

keys.mli
@@ -1,0 +1,19 @@
+type t =
+    Ascii of char
+  | Code of int
+  | Ctrl of int
+  | Fn of int
+  | Backspace
+  | Delete
+  | Escape
+  | Insert
+  | Enter
+  | Up
+  | Down
+  | Left
+  | Right
+  | Next
+  | Prior
+  | Home
+  | End
+val to_string : t -> string

lablGL/gl.mli
@@ -1,0 +1,64 @@
+(* $Id: gl.mli,v 1.23 2012-03-06 03:31:02 garrigue Exp $ *)
+
+(* Exceptions *)
+
+exception GLerror of string
+
+(* Types common to all modules *)
+
+type rgb = float * float * float
+type rgba = float * float * float * float
+
+type point2 = float * float
+type point3 = float * float * float
+type point4 = float * float * float * float
+type vect3 = float * float *float
+
+type clampf = float
+type short = int
+type kind = [`bitmap|`byte|`float|`int|`short|`ubyte|`uint|`ushort]
+type real_kind = [`byte|`float|`int|`short|`ubyte|`uint|`ushort]
+
+type format =
+  [`alpha|`bgr|`bgra|`blue|`color_index|`depth_component|`green|`luminance
+  |`luminance_alpha|`red|`rgb|`rgba|`stencil_index]
+val format_size : [< format] -> int
+
+type target =
+  [`color_4|`index|`normal|`texture_coord_1|`texture_coord_2|`texture_coord_3
+  |`texture_coord_4|`trim_2|`trim_3|`vertex_3|`vertex_4]
+val target_size : [< target] -> int
+
+type cmp_func =
+  [`always|`equal|`gequal|`greater|`lequal|`less|`never|`notequal]
+type face = [`back|`both|`front]
+
+(* Basic functions *)
+
+val flush : unit -> unit
+val finish : unit -> unit
+
+type cap =
+  [`alpha_test|`auto_normal|`blend|`clip_plane0|`clip_plane1|`clip_plane2
+  |`clip_plane3|`clip_plane4|`clip_plane5|`color_material|`cull_face
+  |`depth_test|`dither|`fog|`light0|`light1|`light2|`light3|`light4|`light5
+  |`light6|`light7|`lighting|`line_smooth|`line_stipple
+  |`index_logic_op |`color_logic_op
+  |`map1_color_4|`map1_index|`map1_normal|`map1_texture_coord_1
+  |`map1_texture_coord_2|`map1_texture_coord_3|`map1_texture_coord_4
+  |`map1_vertex_3|`map1_vertex_4|`map2_color_4|`map2_index|`map2_normal
+  |`map2_texture_coord_1|`map2_texture_coord_2|`map2_texture_coord_3
+  |`map2_texture_coord_4|`map2_vertex_3|`map2_vertex_4|`normalize|`point_smooth
+  |`polygon_offset_fill|`polygon_offset_line|`polygon_offset_point
+  |`polygon_smooth|`polygon_stipple|`scissor_test|`stencil_test|`texture_1d
+  |`texture_2d|`texture_gen_q|`texture_gen_r|`texture_gen_s|`texture_gen_t]
+val enable : cap -> unit
+val disable : cap -> unit
+val is_enabled : cap -> bool
+
+type error =
+  [`no_error|`invalid_enum|`invalid_value|`invalid_operation
+  |`stack_overflow|`stack_underflow|`out_of_memory|`table_too_large]
+val get_error : unit -> error
+val raise_error : string -> unit
+  (* raise GLerror if there is a current error, otherwise do nothing *)

lablGL/glArray.mli
@@ -1,0 +1,62 @@
+(** Vertex array manipulation functions *)
+(* $Id: glArray.mli,v 1.7 2008-10-25 02:22:58 garrigue Exp $ *)
+
+(** The six different kinds for array *)
+type kind =
+  [ `color | `edge_flag | `index | `normal | `texture_coord | `vertex ]
+
+(** Tell openGL the address of the edgeFlag array.
+    Raw array must be static. *)
+val edge_flag : [ `bitmap ] Raw.t -> unit
+
+(** Tell openGL the address of the texCoor array
+    Raw array must be static. *)
+val tex_coord :
+  [< `one | `two | `three | `four] -> 
+  [< `double | `float | `int | `short ] Raw.t -> unit
+
+(** Tell openGL the address of the color array
+    Raw array must be static. *)
+val color :
+  [< `three | `four] ->
+  [< `byte | `double | `float | `int | `short | `ubyte | `uint | `ushort ]
+  Raw.t -> unit
+
+(** Tell openGL the address of the index array
+    Raw array must be static. *)
+val index : [< `double | `float | `int | `short | `ubyte ] Raw.t -> unit
+
+(** Tell openGL the address of the normal array
+    Raw array must be static. *)
+val normal : [< `byte | `double | `float | `int | `short ] Raw.t -> unit
+
+(** Tell openGL the address of the vertex array
+    Raw array must be static. *)
+val vertex :
+  [< `two | `three | `four] -> [< `double | `float | `int | `short ] Raw.t 
+  -> unit
+
+(** Tell openGL the address of to use the specified array
+    Raw array must be static. *)
+external enable : kind -> unit = "ml_glEnableClientState"
+
+(** Tell openGL the address of not to use the specified array
+    Raw array must be static. *)
+external disable : kind -> unit = "ml_glDisableClientState"
+
+(* GlArray.element i
+   sends to openGL the element i of all enabled arrays *)
+external element : int -> unit = "ml_glArrayElement"
+
+(* GlArray.draw_arrays shape i c
+   sends to openGL a GlDraw.begins shape and all the element from i to i+c-1 
+   of all enabled arrays and finally do a GlDraw.ends () *)
+external draw_arrays : GlDraw.shape -> first:int -> count:int -> unit
+  = "ml_glDrawArrays"
+
+(* GlArray.draw_elements shape c tbl
+   sends to openGL a GlDraw.begins shape and all the element from tbl[0] to 
+   tbl[c-1] of all enabled arrays and finally do a GlDraw.ends () *)
+external draw_elements :
+  GlDraw.shape -> count:int -> [< `ubyte | `uint | `ushort ] Raw.t -> unit
+  = "ml_glDrawElements"

lablGL/glClear.mli
@@ -1,0 +1,12 @@
+(* $Id: glClear.mli,v 1.3 1999-11-15 09:55:05 garrigue Exp $ *)
+
+type buffer = [`accum|`color|`depth|`stencil]
+val clear : buffer list -> unit
+    (* glClear: clear the specified buffers *)
+
+val accum : ?alpha:float -> Gl.rgb -> unit
+val color : ?alpha:float -> Gl.rgb -> unit
+val depth : Gl.clampf -> unit
+val index : float -> unit
+val stencil : int -> unit
+    (* Set the clear value for each buffer: glClearAccum etc *)

commit d5fbadcf4d591597baf9c89901d7e573ecab030e

Fix dependency regression introduced in c6fc00b5e88813e0510b2ae76fb838c21648d36d

build.bash
@@ -170,7 +170,7 @@ bocaml1() {
     # using a cache is correct but slow(er (much)) way to handle this.
     while read d; do
         bocaml $d $((n+1))
-        deps+=" $d"
+        deps+=" $outd/$d"
     done <$o.depl
 
     cmd="ocamlc $(oflags $o) -c -o $o $s"