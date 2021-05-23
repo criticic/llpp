d057d3488cc2badd2cd917c840091f33dfce0616
malc <clamky@hotmail.com>
2021-05-23 22:59:52 +0300

    Continue fighting implicit creation of compiled interface files
    By (sadly) not using wsi.mli at all. ocamlc refuses to play ball when
    interface is not in the same directory with the implementation [1],
    and symlinking:
    
      causes cognitive dissonance
      is fragile in presence of PBMAC
    
    [1] misc/notes/cmi.sh describes the problem (removed by this commit)

--- misc/notes/cmi.sh
+++ misc/notes/cmi.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -eu
 
-# On why wsi.mli is symlinked from the root to wsi/_platform_/wsi.mli
-
 mkdir -p /tmp/cmi/{d,b/d}
 cd /tmp/cmi
 touch a.mli d/a.ml
 ocamlc -c -I b/ -o b/a.cmi a.mli
 ocamlc -c -I b/ -o b/d/a.cmo d/a.ml
 
-# after running this script a.cmi will be in b/ and in b/d
+# after running this script a.cmi will be in b/ _AND_ in b/d
