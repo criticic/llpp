7b2a30d65e536ae0a72f02e88c049e43151ee136
malc <clamky@hotmail.com>
2021-07-21 14:21:52 +0300

    Sync with upstream

--- misc/getmupdf.sh
+++ misc/getmupdf.sh
@@ -3,7 +3,7 @@ set -eu
 
 MUPDF_OUTPUT_DIR="$1"
 MUPDF_URL="git://git.ghostscript.com/mupdf"
-MUPDF_DESIRED_VERSION="983e8ab14c752bf11aa243faeb03da9dc64eed54"
+MUPDF_DESIRED_VERSION="3ba8a4cd71e9ca527c5cb67bd23b95f04c6a2584"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
