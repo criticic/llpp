7ddf1a47a5a25db50b597ced33e1ee0bfc51737f
malc <clamky@hotmail.com>
2021-09-27 09:24:41 +0300

    Utilize OCaml 4.13.0

--- build.bash
+++ build.bash
@@ -104,8 +104,8 @@ mflags() {
 }
 
 overs=$(ocamlc -vnum 2>/dev/null) || overs=""
-if test "$overs" != "4.13.0~rc2"; then
-    url=https://caml.inria.fr/pub/distrib/ocaml-4.13/ocaml-4.13.0~rc2.tar.xz
+if test "$overs" != "4.13.0"; then
+    url=https://caml.inria.fr/pub/distrib/ocaml-4.13/ocaml-4.13.0.tar.xz
     txz=$outd/$(basename $url)
     keycmd="printf $url; digest $txz;"
     isfresh $txz "$(eval $keycmd)" || {
