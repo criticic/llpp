6dea5ddc4e06641d429125bfb755d742a3a11c71
malc <clamky@hotmail.com>
2021-10-05 20:53:24 +0300

    Sync with upstream
    This includes fix for tesseractless builds.

--- misc/getmupdf.sh
+++ misc/getmupdf.sh
@@ -5,7 +5,7 @@ MUPDF_OUTPUT_DIR="$1"
 #u="git://git.ghostscript.com/mupdf"
 u="https://github.com/ArtifexSoftware/mupdf"
 MUPDF_URL="${2-$u}"
-MUPDF_DESIRED_VERSION="6a21c07fd7b681823112606a1fdfcb201fba286a"
+MUPDF_DESIRED_VERSION="6a48b34df62c2fcccbeab9c3e64865203477b1ed"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
