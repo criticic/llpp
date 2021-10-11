489645fcc568172861d6b9d9718a33d60f104b68
malc <clamky@hotmail.com>
2021-10-11 19:41:03 +0300

    Sync with upstream...
    ...and double check that things _do_ build/run.

--- misc/getmupdf.sh
+++ misc/getmupdf.sh
@@ -5,7 +5,7 @@ MUPDF_OUTPUT_DIR="$1"
 u="git://git.ghostscript.com/mupdf"
 #u="https://github.com/ArtifexSoftware/mupdf"
 MUPDF_URL="${2-$u}"
-MUPDF_DESIRED_VERSION="3f050bc65f0a9ba841b57bbf483c7fe378df7c0e"
+MUPDF_DESIRED_VERSION="ddeedf35775d1aa145d9133517a85643f98aadb8"
 
 if [ ! -d ${MUPDF_OUTPUT_DIR} ]; then
     echo "mupdf does not exist, fetching it from ${MUPDF_URL}"
